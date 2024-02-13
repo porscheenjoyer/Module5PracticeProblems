882893992c5f9396652f72ffbe6d7977ab09150c
Period 6 <braylen.diec@talawanda.org>
2024-02-13 14:04:46 -0500

    204

---
+++ src/Task5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+        //Variables
+        Scanner in = new Scanner(System.in);
+        double userTemp;
+        String trash;
+
+        System.out.println("Please enter the temperature in Fahrenheit");
+
+    }
+}
