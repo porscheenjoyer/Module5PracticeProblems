bdcb3c5dfd0c0293f65428fefd0f987028eb456c
Period 6 <braylen.diec@talawanda.org>
2024-02-14 13:32:05 -0500

    131

--- src/Task5.java
+++ src/Task5.java
@@ -8,7 +8,39 @@ public class Task5 {
         double userTemp;
         String trash;
 
-        System.out.println("Please enter the temperature in Fahrenheit");
+        //collect temp
+        System.out.println("Please enter the temperature of the water in Fahrenheit");
+
+        if (in.hasNextDouble()) {
+
+            userTemp = in.nextDouble();
+
+            if (userTemp >= 212) {
+
+                System.out.println("The waster is boiling and is in a gas state");
+
+            }
+
+            else if (userTemp <= 32) {
+
+                System.out.println("The water is frozen and is a solid");
+
+            }
+
+            else {
+
+                System.out.println("Your water is a liquid");
+
+            }
+
+        }
+
+        else {
+
+            trash = in.nextLine();
+            System.out.println("You gave an invalid value: " + trash);
+
+        }
 
     }
 }
